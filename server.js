f2ad3d9422d00f990e98d6301b9eeb65be4bf91e
Praneethdevaraya2408 <praneethdevaraya2408@gmail.com>
2021-05-04 07:32:59 -0700

    hi

--- server.js
+++ server.js
@@ -7,15 +7,12 @@ app.get("/", function(req, res) {
     res.send("Welcome to PRANEETH Basic Site");
 })
 
-app.get("/noderesume", function(req, res) {
-    let fnam = __dirname + "/frontend/html/noderesume.html";
-    res.sendFile(fnam);
-})
 app.get("/resume", function(req, res) {
     let fnam = __dirname + "/frontend/html/resume.html";
     res.sendFile(fnam);
 })
-app.get("/ggle", function(req, res) {
+
+app.get("/google", function(req, res) {
     let fnam = __dirname + "/google/index.html";
     res.sendFile(fnam);
 })
