1b44c7b7733285160afcc0a81b49814987995ea3
Praneethdevaraya2408 <praneethdevaraya2408@gmail.com>
2021-06-08 20:06:02 -0700

    new commit

---
+++ backend/models/usermodel.js
@@ -0,0 +1,37 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const userSchema=new Schema({
+    fname:{
+        type:String,
+        required:true
+
+    },
+    lname:{
+        type:String,
+        required:true
+
+    },
+    uname:{
+        type:String,
+        required:true
+
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phno:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    cpassword:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+const User=mongoose.model('User',userSchema);
+module.exports=User;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const bcrypt=require('bcryptjs');
+const User=require('../FULL_STACK_BZ/backend/models/usermodel');
 const app = express();
 var mongoose= require('mongoose');
 //const bodyParser = require("body-parser");
@@ -50,18 +51,80 @@ app.get("/todo", function(req, res) {
     let fnam = __dirname + "/frontend/html/course.html";
     res.sendFile(fnam);
 })
-app.get('/:page', function(req, res){
-    var ext = path.extname(req.params.page);
-    // console.log(ext);
-    if(ext=="")
-    res.sendFile(__dirname+ '/frontend/html/'+ req.params.page+".html");
-    //else  res.sendFile(__dirname+ '/frontend/'+ req.params.page);
-})
+
 app.get("/todolocal", function(req, res) {
     let fnam = __dirname + "/frontend/html/todo_local.html";
     res.sendFile(fnam);
 })
 
+app.get("/register", function(req, res) {
+    let fnam = __dirname + "/frontend/html/registration.html";
+    res.sendFile(fnam);
+})
+app.get("/login", function(req, res) {
+    let fnam = __dirname + "/frontend/html/login.html";
+    res.sendFile(fnam);
+})
+app.get("/host", function(req, res) {
+    let fnam = __dirname + "/frontend/html/host.html";
+    res.sendFile(fnam);
+})
+app.get("/dashboard", function(req, res) {
+    let fnam = __dirname + "/frontend/html/dashboard.html";
+    res.sendFile(fnam);
+})
+
+app.post("/register",(req,res)=>{
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var uname = req.body.uname;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var password = req.body.password;
+    var cpassword = req.body.cpassword;
+    if (password===cpassword) {
+        var data = {
+            "fname": fname,
+            "lname": lname,
+            "uname": uname,
+            "email" : email,
+            "phno": phno,
+            "password" : password,
+            "cpassword" :cpassword
+        
+        }
+        User.insertMany(data,(err,collection)=>{
+            if(err){
+                throw err;
+            }
+           console.log(data)
+        }); return res.redirect('/host')
+    
+        
+    } else {
+        res.redirect("/newone")
+    }
+
+})
+
+app.post('/login',(req,res)=>{
+    const uname=req.body.uname;
+    const password=req.body.password;
+    const username=User.findOne({uname:uname},function(err,resu){
+        if (err) {
+            console.log('unsuccessful')
+        } else {
+            if(resu.password===password)
+            {
+                res.redirect('/dashboard')
+            }
+            else{
+                res.redirect("/newone2")
+            }
+        }
+
+    });
+})
 const PORT = process.env.PORT || 3020;
 
 // Start the server
