1eea1624e9266245a0431814363ff8af915f3fc6
Praneethdevaraya2408 <praneethdevaraya2408@gmail.com>
2021-06-15 22:17:55 -0700

    final

--- BZ_FLOW/app.js
+++ BZ_FLOW/app.js
@@ -3,13 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var MongoStore=require('connect-mongo');
+var config=require('./Backend/Config/Config')
 var dbconnect=require('./Backend/Lib/DB_connection_Lib');
 dbconnect.connect();
+var session=require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 require('./Backend/Lib/DBusersBootstrap').createUsers();
 var app = express();
 var userRouter=require('./routes/users')
+app.use(session({
+    resave:false, 
+    saveUninitialized:false, 
+    secret:config.session_secret, 
+    store: MongoStore.create({ mongoUrl: config.mongo_connection_string })
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
